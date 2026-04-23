9e456bccac679573ceff80ddf2134d2e507a884b
Lucas Isensee <lucas63466586@alunos.sc.senac.br>
2026-04-23 20:35:22 -0300

    listagem-e-exibicao

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=== Listagem e exibição dos usuários ===\n");
+Console.Write("Digite um nome de usuário: ");
+string nome = Console.ReadLine();
+
+if (nome != null)
+{
+    Console.WriteLine($"\nNome de usuário encontrado, Bem vindo {nome}!");
+}
+else if (nome == null)
+{
+    Console.WriteLine("Nome de usuário INVÁLIDO, tente novamente!");
+}
