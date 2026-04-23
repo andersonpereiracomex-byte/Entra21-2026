7a507277f69acb9aad3f166bcf5787a10b03658d
kablumenschein <ka.blumenschein@hotmail.com>
2026-04-23 20:55:37 -0300

    removi uma opção no menu

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -2,14 +2,13 @@ string[] nomes = new string[0];
 int totalUsuario = 0;
 string opcao = "";
 
-while (opcao != "5")
+while (opcao != "4")
 {
     Console.WriteLine("====== SISTEMA DE USUÁRIOS ======");
     Console.WriteLine("1 - Cadastrar usuário");
     Console.WriteLine("2 - Listar usuários");
-    Console.WriteLine("3 - Buscar usuário");
-    Console.WriteLine("4 - Remover usuário");
-    Console.WriteLine("5 - Sair");
+    Console.WriteLine("3 - Buscar/Remover usuário");
+    Console.WriteLine("4 - Sair");
     Console.Write("Escolha uma opção: ");
     opcao = Console.ReadLine();
 }
